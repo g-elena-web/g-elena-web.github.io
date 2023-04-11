3345a41c003e54e32e75127b0457564eb42d9e3e
Elena <g.elena.web@gmail.com>
2023-04-11 22:41:57 +0300

    Update skills, add new font, disable selection in logo

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -8,17 +8,21 @@ function About() {
             <h3>About Me</h3>
             <h4>Skills</h4>
             <ul>
-                <li><i className="fa-brands fa-html5"></i> HTML/HTML5</li>
-                <li><i className="fa-brands fa-css3-alt"></i> CSS/CSS3</li>
+                <li><i className="fa-brands fa-html5"></i> HTML</li>
+                <li><i className="fa-brands fa-css3-alt"></i> CSS</li>
                 <li><i className="fa-brands fa-square-js"></i> JavaScript</li>
                 <li><i className="fa-brands fa-react"></i> React</li>
+                <li><i className="fa-brands fa-vuejs"></i> Vue.js</li>
                 <li><i className="fa-brands fa-sass"></i> SASS/SCSS</li>
                 <li><i className="fa-brands fa-git"></i> Git</li>
+                <li><i className="fa-brands fa-gulp"></i> Gulp</li>
                 <li><i className="fa-solid fa-dollar-sign"></i> jQuery</li>
                 <li><i className="fa-brands fa-bootstrap"></i> Bootstrap</li>
+                <li><i className="fa-solid fa-cube"></i> BEM</li>
+                <li><i className="fa-brands fa-jira"></i> Jira</li>
                 <li><i className="fa-solid fa-wand-magic-sparkles"></i> Adobe Photoshop</li>
                 <li><i className="fa-solid fa-window-restore"></i> Responsive Web Design</li>
-                <li><i className="fa-solid fa-database"></i> SQL basics</li>
+                <li><i className="fa-solid fa-database"></i> SQL</li>
                 <li><i className="fa-brands fa-npm"></i> npm</li>
             </ul>
             <h4>Languages</h4>
